dc9ad3e7a68b20b4b584c382e318ff0cda0d5e93
keva4ko <119354395+keva4ko@users.noreply.github.com>
2022-12-04 22:35:04 +0100

    Update upload.js
    Anpassung Text

--- plugins/upload.js
+++ plugins/upload.js
@@ -3,12 +3,12 @@ bot(
 	{
 		pattern: 'upload ?(.*)',
 		fromMe: true,
-		desc: 'Download from url',
+		desc: 'Download vom Link',
 		type: 'download',
 	},
 	async (message, match) => {
 		match = isUrl(match || message.reply_message.text)
-		if (!match) return await message.send('_Example : upload url_')
+		if (!match) return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\n_Beispiel:_\n upload [Link]')
 		if (match.startsWith('https://images.app.goo.gl'))
 			match = await getImgUrl(match)
 		await message.sendFromUrl(match, { buffer: false })
