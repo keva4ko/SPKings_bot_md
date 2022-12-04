d311e0eacadee5845cc8496925dc10e9cc212738
keva4ko <119354395+keva4ko@users.noreply.github.com>
2022-12-04 21:52:26 +0100

    Update pinterest.js
    Anpassung Text

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -4,15 +4,15 @@ bot(
 	{
 		pattern: 'pinterest ?(.*)',
 		fromMe: true,
-		desc: 'Download pinterest video/image',
+		desc: 'Download pinterest Video/Bilder',
 		type: 'download',
 	},
 	async (message, match) => {
 		match = isUrl(match || message.reply_message.text)
-		if (!match) return await message.send('_Example : pinterest url_')
+		if (!match) return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\nLade Bilder oder Videos aus Pinterest herunter\nBeispiel: pinterest [Link]')
 		const result = await pinterest(match)
 		if (!result.length)
-			return await message.send('*Not found*', {
+			return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\n_Sicher das ich da was finden sollte?_🙄', {
 				quoted: message.quoted,
 			})
 		return await message.sendFromUrl(result)
