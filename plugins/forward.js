b9f52c433ed5e5e7d0b4fecd8f3c9484ecb28364
keva4ko <119354395+keva4ko@users.noreply.github.com>
2022-12-04 20:20:14 +0100

    Update forward.js
    Anpassung Text

--- plugins/forward.js
+++ plugins/forward.js
@@ -4,12 +4,12 @@ bot(
 	{
 		pattern: 'forward ?(.*)',
 		fromMe: true,
-		desc: 'forward replied msg',
+		desc: 'Markierte Nachricht weiterleiten',
 		type: 'misc',
 	},
 	async (message, match) => {
 		if (!message.reply_message)
-			return await message.send('⭐*SPKings*⭐\n\n*Markiere eine Nachricht*')
+			return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\n*Markiere eine Nachricht*')
 		for (const jid of parsedJid(match)) await forwardOrBroadCast(jid, message)
 	}
 )
@@ -18,12 +18,12 @@ bot(
 	{
 		pattern: 'save ?(.*)',
 		fromMe: true,
-		desc: 'forward replied msg to u',
+		desc: 'Markierte Nachricht an mich weiterleiten',
 		type: 'misc',
 	},
 	async (message, match) => {
 		if (!message.reply_message)
-			return await message.send('⭐*SPKings*⭐\n\n*Markiere eine Nachricht*')
+			return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\n*Markiere eine Nachricht*')
 		await forwardOrBroadCast(message.participant, message)
 	}
 )
