a5bc1bd44506fa4ee003dd9c9a3a388afeda13ab
keva4ko <119354395+keva4ko@users.noreply.github.com>
2022-12-04 20:22:20 +0100

    Update google.js
    Anpassung Text

--- plugins/google.js
+++ plugins/google.js
@@ -9,14 +9,14 @@ bot(
 	},
 	async (message, match) => {
 		if (!message.reply_message || !message.reply_message.image)
-			return await message.send('⭐*SPKings*⭐\n\n*Markiere ein Bild*')
+			return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\n*Markiere ein Bild*')
 		const result = await googleImageSearch(
 			await getUrl(
 				await message.reply_message.downloadAndSaveMediaMessage('google')
 			),
 			'ris'
 		)
-		if (!result.length) return await message.send('⭐*SPKings*⭐\n\n_Ich habe nichts gefunden_')
+		if (!result.length) return await message.send('⭐   _*Blvck Squad the Bot*_   ⭐\n\n_Ich habe nichts gefunden_🤷‍♂️')
 		return await message.send(result.join('\n'), {
 			quoted: message.data,
 		})
